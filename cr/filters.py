2a734bdf60b94de69c3bbf9de9e2e6e16e42cbb3
廖一銘 <leeliao@why-not.com.tw>
2021-05-19 19:28:31 +0800

    add reset button in detail template

--- cr/filters.py
+++ cr/filters.py
@@ -1,11 +1,15 @@
 from django.forms import widgets
 import django_filters
 from django_filters import DateFromToRangeFilter
+from django_filters.widgets import RangeWidget
 from .models import Task
 
 
 class TaskFilter(django_filters.FilterSet):
-    filter_date = DateFromToRangeFilter(field_name='cr_date', label="")
+    filter_date = DateFromToRangeFilter(label="", 
+                                        field_name='cr_date', 
+                                        widget=RangeWidget(attrs={'placeholder': 'YYYY-MM-DD',
+                                                                  'type': 'date'}))
     class Meta:
         model = Task
         fields = ['cr_date']
